4a111d3b75ae6cd053aae8dce752a0180cc833d2
Anthony D <dunahee@gmail.com>
2019-04-11 11:32:00 -0500

    birthday function return number of times chocolate bar can be divided based on criteria

--- JavaScript/HackerRank.js
+++ JavaScript/HackerRank.js
@@ -181,12 +181,13 @@ function breakingRecords(scores) {
     var recordChanges = [0, 0];
     //checks each score to see if it breaks a record
     scores.forEach(function (c) {
-        if (c < records[0]) {
+        // checking to see if game score is a new max or new min
+        if (c > records[0]) {
             records.shift();
             records.unshift(c);
             return recordChanges[0]++;
         }
-        else if (c > records[1]) {
+        else if (c < records[1]) {
             records.pop();
             records.push(c);
             return recordChanges[1]++;
@@ -196,3 +197,31 @@ function breakingRecords(scores) {
 }
 exports.breakingRecords = breakingRecords;
 ;
+/* Lily has a chocolate bar that she wants to share it with Ron for his birthday.
+ Each of the squares has an integer on it. She decides to share a contiguous
+ segment of the bar selected such that the length of the segment matches Ron's
+  birth month and the sum of the integers on the squares is equal to his birth
+   day. You must determine how many ways she can divide the chocolate.
+
+Function Description
+
+Complete the birthday function in the editor below. It should return an integer
+ denoting the number of ways Lily can divide the chocolate bar.
+
+birthday has the following parameter(s):
+
+s: an array of integers, the numbers on each of the squares of chocolate
+d: an integer, Ron's birth day
+m: an integer, Ron's birth month*/
+function birthday(s, d, m) {
+    var divisions = 0;
+    for (var i = 0; i < s.length; i++) {
+        var segmentSum = s.slice(i, i + m)
+            .reduce(function (acc, c) { return acc + c; });
+        if (segmentSum === d) {
+            divisions++;
+        }
+    }
+    return divisions;
+}
+exports.birthday = birthday;
--- JavaScript/HackerRank.ts
+++ JavaScript/HackerRank.ts
@@ -192,3 +192,31 @@ export function breakingRecords(scores: Array<number>) {
     });
     return recordChanges;
 };
+
+/* Lily has a chocolate bar that she wants to share it with Ron for his birthday.
+ Each of the squares has an integer on it. She decides to share a contiguous 
+ segment of the bar selected such that the length of the segment matches Ron's
+  birth month and the sum of the integers on the squares is equal to his birth
+   day. You must determine how many ways she can divide the chocolate.
+
+Function Description
+
+Complete the birthday function in the editor below. It should return an integer
+ denoting the number of ways Lily can divide the chocolate bar.
+
+birthday has the following parameter(s):
+
+s: an array of integers, the numbers on each of the squares of chocolate
+d: an integer, Ron's birth day
+m: an integer, Ron's birth month*/
+export function birthday(s: Array<number>, d: number, m: number) {
+    let divisions: number = 0;
+    for (let i: number = 0; i < s.length; i++) {
+        const segmentSum: number = s.slice(i, i + m)
+            .reduce((acc, c) => acc + c);
+        if (segmentSum === d) {
+            divisions++;
+        }
+    }
+    return divisions;
+}
